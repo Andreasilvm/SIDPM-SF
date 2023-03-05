e209259d7dc4d88aa1eef9e871fb61386777b7d7
Andrea Silva <yuliandreasilvamunoz@gmail.com>
2023-03-05 14:26:31 -0500

    Agregar archivos de proyecto.

---
+++ SIDPM/MainActivity.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace SIDPM
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText txtUsuario;
+        EditText txtContraseña;
+        Button btnBuscar;
+        Button btnRegistrar;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            txtUsuario = FindViewById<EditText>(Resource.Id.txtUsuario);
+            txtContraseña = FindViewById<EditText>(Resource.Id.txtContraseña);
+            btnBuscar = FindViewById<Button>(Resource.Id.txtContraseña);
+            btnRegistrar = FindViewById<Button>(Resource.Id.btnRegistrarse);
+
+            btnBuscar.Click += btnBuscar_Click;
+            btnRegistrar.Click += BtnRegistrar_Click;
+
+        }
+
+        private void BtnRegistrar_Click(object sender, System.EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(RegistrarActivity));
+            StartActivity(i);
+        }
+
+        private void btnBuscar_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                Login resultado = null;
+                if (!string.IsNullOrEmpty(txtUsuario.Text.Trim()) && !string.IsNullOrEmpty(txtContraseña.Text.Trim()))
+                {
+                    resultado = new Auxiliar().SelecionarUno(txtUsuario.Text.Trim(), txtContraseña.Text.Trim());
+                    if (resultado != null)
+                    {
+                        txtUsuario.Text = resultado.Usuario.ToString();
+                        Toast.MakeText(this, "Login realizado con exito", ToastLength.Short).Show();
+                        var bienvenido = new Intent(this, typeof(MainActivity));// modificar despues
+                        bienvenido.PutExtra("usuario", FindViewById<EditText>(Resource.Id.txtUsuario).Text);
+                        StartActivity(bienvenido);
+                        Finish();
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Nombre de usuario y/o clave inválida(s)", ToastLength.Long).Show();
+                    }
+                }
+                else
+                {
+                    Toast.MakeText(this, "Nombre de usuario y/o clave son vacios", ToastLength.Long).Show();
+                }
+
+            }
+            catch { }
+        }
+
+
+    }
+}
---
+++ SIDPM/RegistrarActivity.cs
@@ -0,0 +1,67 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIDPM
+{
+    [Activity(Label = "RegistrarActivity")]
+    public class RegistrarActivity : Activity
+    {
+        // se crean los objetos 
+        EditText txtNuevoUsuario;
+        EditText txtcedula;
+        EditText txtPrimerNombre;
+        EditText txtSegundoNombre;
+        EditText txtPrimerApellido;
+        EditText txtSegundoApellido;
+        EditText txtPrograma;
+        EditText txtNuevaContraseña;
+        Button BtnRegistrarUsuario;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            txtNuevoUsuario = FindViewById<EditText>(Resource.Id.txtNuevoUsuario);
+            txtcedula = FindViewById<EditText>(Resource.Id.txtcedula);
+            txtPrimerNombre = FindViewById<EditText>(Resource.Id.txtPrimerNombre);
+            txtSegundoNombre = FindViewById<EditText>(Resource.Id.txtSegundoNombre);
+            txtPrimerApellido = FindViewById<EditText>(Resource.Id.txtPrimerApellido);
+            txtSegundoApellido = FindViewById<EditText>(Resource.Id.txtSegundoApellido);
+            txtPrograma = FindViewById<EditText>(Resource.Id.txtPrograma);
+            txtNuevaContraseña = FindViewById<EditText>(Resource.Id.txtNuevaContraseña);
+            BtnRegistrarUsuario = FindViewById<Button>(Resource.Id.BtnRegistrarUsuario);
+
+            BtnRegistrarUsuario.Click += BtnRegistrarUsuario_Click;
+        }
+
+        private void BtnRegistrarUsuario_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txtNuevoUsuario.Text.Trim()) && !string.IsNullOrEmpty(txtNuevaContraseña.Text.Trim()))
+                {
+                    new Auxiliar().Guardar(new Login() { Id = 0, Usuario = txtNuevoUsuario.Text.Trim(), Password = txtNuevaContraseña.Text.Trim() });
+                    Toast.MakeText(this, "Registro guardado", ToastLength.Long).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Por favor ingrese un nombre de usuario y una contraseña", ToastLength.Long).Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, ex.ToString(), ToastLength.Short).Show();
+            }
+        }
+
+
+    }
+}
